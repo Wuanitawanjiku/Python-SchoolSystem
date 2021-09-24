62bd10a96a35178e6bace25d9b9c2038386f610c
Wuanitawanjiku <wanjikuwuanita@gmail.com>
2021-09-24 07:56:58 +0300

    edits

--- SchoolSystem/settings.py
+++ SchoolSystem/settings.py
@@ -141,15 +141,12 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'SchoolSystem/static')]
-
 MEDIA_URL ='/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media/images/')
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
